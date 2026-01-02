105c3a5e9e71100a1ef3baa1415163a7d1b1d161
Wito982 <alvaro_palacios@outlook.com>
2026-01-02 16:02:03 -0300

    remove fk

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -10,9 +10,9 @@ import (
 type Enrollment struct {
 	ID        string         `json:"id" gorm:"type:char(36);not null; primaryKey;unique_index"`
 	UserID    string         `json:"user_id,omitempty" gorm:"type:char(36)"`
-	User      *User          `json:"user,omitempty"`
+	User      *User          `json:"user,omitempty" gorm:"-"`
 	CourseID  string         `json:"course_id" gorm:"type:char(36)"`
-	Course    *Course        `json:"course,omitempty"`
+	Course    *Course        `json:"course,omitempty" gorm:"-"`
 	Status    string         `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time     `json:"-"`
 	UpdatedAt *time.Time     `json:"-"`
